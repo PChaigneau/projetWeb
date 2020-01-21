4890efe7069b9f606f05605c8209abb19c474ed1
PChaig <pichaigneau@laposte.net>
2020-01-21 09:52:57 +0100

    inscription

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { LoginService } from '../Shared/login.service';
+import { LoginService } from '../shared/login.service';
 
 @Component({
   selector: 'app-navbar',
